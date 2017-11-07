e1b6ae6a88ecd89d34e8ae8ecc1c04776488b303
aBluescreen <33433997+aBluescreen@users.noreply.github.com>
2017-11-07 15:46:28 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ function newNum() {
 }
 
 function newColor() {
-  var canvas = document.getElementById("canvas");
+  var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "blue";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
