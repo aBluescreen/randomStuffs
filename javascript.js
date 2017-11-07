68e6f3d99f004c3b113d18456af8a2741222575e
aBluescreen <33433997+aBluescreen@users.noreply.github.com>
2017-11-07 16:37:01 -0600

    rand color test

--- javascript.js
+++ javascript.js
@@ -6,6 +6,8 @@ function newNum() {
 function newColor() {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext("2d");
-  ctx.fillStyle = "blue";
+  var colorVals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+  var randColor = Math.floor(Math.random()*colorVals.length);
+  ctx.fillStyle = "#"+ new colorVals[randColor] + new colorVals[randColor] + new colorVals[randColor] + new colorVals[randColor] + new colorVals[randColor] + new colorVals[randColor];
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
