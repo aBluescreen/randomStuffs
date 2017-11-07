02dfbde0899bd8a97c3d75f1063b21158a5c13eb
aBluescreen <33433997+aBluescreen@users.noreply.github.com>
2017-11-07 15:37:29 -0600

    random color test

--- javascript.js
+++ javascript.js
@@ -2,3 +2,10 @@ function newNum() {
   var randomNum = Math.floor(Math.random()*100);
   document.getElementById('numDisplay').innerHTML = randomNum;
 }
+
+function newColor() {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "blue";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
